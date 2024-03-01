90204e2896571463c4c073520bc2c35064ec9078
246749 <246749@usn.no>
2024-03-01 19:13:37 +0100

    comment

--- Program.cs
+++ Program.cs
@@ -4,18 +4,19 @@
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello, World!");
+            // Prompt the user to enter the number of cards to pick
+            Console.Write("Enter the number of cards to pick: ");
 
-            Console.Write("Enter the nuber if cards to pick: ");
-
+            // Read the input from the user
             string line = Console.ReadLine();
 
-
-            // int.TryParse to convert string to int 
-
+            // Try to parse the input string into an integer using int.TryParse
             if (int.TryParse(line, out int numberOfCards))
             {
+                // If parsing is successful, call CardPicker.PickSomCards to pick the cards
                 string[] cards = CardPicker.PickSomCards(numberOfCards);
+
+                // Iterate over the picked cards array and print each card
                 for (int i = 0; i < cards.Length; i++)
                 {
                     Console.WriteLine(cards[i]);
@@ -23,8 +24,9 @@
             }
             else
             {
+                // If parsing fails, notify the user to enter a valid number
                 Console.WriteLine("Please enter a valid number.");
             }
         }
     }
-    }
+}
