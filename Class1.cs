4bc7c70f63b8f09cc7b4a0f455edc542827b78d2
246749 <246749@usn.no>
2024-03-01 19:03:59 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicksRandomCards
+{
+    internal class CardPicker
+    {
+
+        static Random random = new Random();
+
+        public static string[] PickSomCards(int numberOfCards)
+        {
+
+            string[] peckedCards = new string[numberOfCards];
+
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                peckedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return peckedCards;
+        }
+
+       
+
+        private static string RandomSuit()
+        { 
+
+            //Get a random number from 1 to 5
+            int value = random.Next(1,5);
+
+            if (value == 1) return "Spades";
+
+            if (value == 2) return "Hearts";
+
+            if (value == 3) return "Clubs";
+
+            return "Diamonds";
+            
+
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+
+            if (value == 1) return " ACE";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
+
+    
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PicksRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+
+            Console.Write("Enter the nuber if cards to pick: ");
+
+            string line = Console.ReadLine();
+
+
+            // int.TryParse to convert string to int 
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                string[] cards = CardPicker.PickSomCards(numberOfCards);
+                for (int i = 0; i < cards.Length; i++)
+                {
+                    Console.WriteLine(cards[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+    }
+    }
